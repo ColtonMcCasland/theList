8db5d996e8995d64085107764d6b890686f456db
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-01 19:03:20 -0500

    Initial Commit

---
+++ Kitchen_Sync/Kitchen_SyncApp.swift
@@ -0,0 +1,20 @@
+//
+//  Kitchen_SyncApp.swift
+//  Kitchen_Sync
+//
+//  Created by Colton McCasland on 7/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Kitchen_SyncApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
