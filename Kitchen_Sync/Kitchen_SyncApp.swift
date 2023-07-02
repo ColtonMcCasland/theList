20a3f9b343f8bdf69087bf4e3f06fdcab7d6f651
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-01 19:09:37 -0500

    Changed to CoreDate method

--- Kitchen_Sync/Kitchen_SyncApp.swift
+++ Kitchen_Sync/Kitchen_SyncApp.swift
@@ -13,6 +13,7 @@ struct Kitchen_SyncApp: App {
 
     var body: some Scene {
         WindowGroup {
+            //
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
