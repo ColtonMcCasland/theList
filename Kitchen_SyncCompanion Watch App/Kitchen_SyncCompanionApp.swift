90ebb70af33e45b94c9c8a74f12073307918ff69
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-02 14:13:39 -0500

    Adding watch version of app

---
+++ Kitchen_SyncCompanion Watch App/Kitchen_SyncCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kitchen_SyncCompanionApp.swift
+//  Kitchen_SyncCompanion Watch App
+//
+//  Created by Colton McCasland on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Kitchen_SyncCompanion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
