f0e549fcb98e612020e54d74f697181e6f9045a0
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-04 12:13:45 -0500

    reset watch files

--- Kitchen_SyncWatchOS/Views/ListView.swift
+++ Kitchen_SyncWatchOS/Views/ListView.swift
@@ -8,24 +8,25 @@
 import SwiftUI
 
 struct ListView: View {
-    var items: [watchItem]
+//    var items: [watchItem]
 
     var body: some View {
-        List(items) { item in
-            VStack(alignment: .leading) {
-                Text(item.name)
-                Text(item.description)
-            }
-        }
+        Text("List")
+//        List(items) { item in
+//            VStack(alignment: .leading) {
+//                Text(item.name)
+//                Text(item.description)
+//            }
+//        }
     }
 }
 
-struct ListView_Previews: PreviewProvider {
-    static var previews: some View {
-        ListView(items: [
-            watchItem(name: "Item 1", description: "Description 1"),
-            watchItem(name: "Item 2", description: "Description 2"),
-            watchItem(name: "Item 3", description: "Description 3")
-        ])
-    }
-}
+//struct ListView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ListView(items: [
+//            watchItem(name: "Item 1", description: "Description 1"),
+//            watchItem(name: "Item 2", description: "Description 2"),
+//            watchItem(name: "Item 3", description: "Description 3")
+//        ])
+//    }
+//}
--- Kitchen_SynciOS/Kitchen_SynciOSApp.swift
+++ Kitchen_SynciOS/Kitchen_SynciOSApp.swift
@@ -6,67 +6,16 @@
 //
 
 import SwiftUI
-import WatchConnectivity
 
 
 @main
 struct Kitchen_SyncApp: App {
     let persistenceController = PersistenceController.shared
-    @StateObject private var connectivityHandler = ConnectivityHandler()
     
     var body: some Scene {
             WindowGroup {
                 ContentView()
                     .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                    .environmentObject(connectivityHandler)
             }
         }
-}
-
-
-class ConnectivityHandler: ObservableObject {
-    private let sessionDelegate = SessionDelegate()
-
-    init() {
-        if WCSession.isSupported() {
-            let session = WCSession.default
-            session.delegate = sessionDelegate
-            session.activate()
-        }
-    }
-
-    func sendToWatchOS(data: [String: Any]) {
-        if WCSession.default.isReachable {
-            do {
-                try WCSession.default.updateApplicationContext(data)
-            } catch {
-                print("Failed to send data to watchOS: \(error)")
-            }
-        } else {
-            print("WatchOS app is not reachable")
-        }
-    }
-}
-
-class SessionDelegate: NSObject, WCSessionDelegate {
-    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        // Handle session activation completion
-    }
-
-    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
-        // Handle received application context from watchOS app
-    }
-
-    // Add other required WCSessionDelegate methods
-    func sessionDidBecomeInactive(_ session: WCSession) {
-        // Handle session becoming inactive
-    }
-
-    func sessionDidDeactivate(_ session: WCSession) {
-        // Handle session deactivation
-    }
-
-    func sessionWatchStateDidChange(_ session: WCSession) {
-        // Handle watch state change
-    }
 }
