344f917bdda843b6406bef35669f6202acf42505
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-22 16:53:57 -0500

    Revert "instantly gets list for watch app"
    This reverts commit cc1954142ebb893fa6cf1b108ba676ddeb537c7e.

--- Kitchen_SyncWatchOS/WatchManager.swift
+++ Kitchen_SyncWatchOS/WatchManager.swift
@@ -16,13 +16,9 @@ class WatchManager: NSObject, WCSessionDelegate, ObservableObject {
         }
     }
     
-    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        // Handle activation completion if needed
-    }
-    
-    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+    func session(_ session: WCSession, didReceiveApplicationContext applicationContext: [String : Any]) {
         DispatchQueue.main.async {
-            self.updateRecords(from: message)
+            self.updateRecords(from: applicationContext)
         }
     }
     
@@ -42,16 +38,35 @@ class WatchManager: NSObject, WCSessionDelegate, ObservableObject {
     }
     
     func toggleIsTapped(for recordIndex: Int) {
-        records[recordIndex].isTapped.toggle()
-        
-        // Send a message to the iOS app to update the corresponding item's isTapped property
-        if WCSession.default.isReachable {
-            let tappedItem = records[recordIndex]
-            let message: [String: Any] = ["updateIsTapped": true, "timestamp": tappedItem.timestamp]
-            WCSession.default.sendMessage(message, replyHandler: nil, errorHandler: { error in
-                // Handle error
-                print("Error sending message to iOS app: \(error.localizedDescription)")
-            })
+            records[recordIndex].isTapped.toggle()
+            
+            // Send a message to the iOS app to update the corresponding item's isTapped property
+            if WCSession.default.isReachable {
+                let tappedItem = records[recordIndex]
+                let message: [String: Any] = ["updateIsTapped": true, "timestamp": tappedItem.timestamp]
+                WCSession.default.sendMessage(message, replyHandler: nil, errorHandler: { error in
+                    // Handle error
+                    print("Error sending message to iOS app: \(error.localizedDescription)")
+                })
+            }
+        }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        // Handle activation completion if needed
+    }
+
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        if let recordsDict = message["records"] as? [[String: Any]] {
+            DispatchQueue.main.async {
+                self.records = recordsDict.compactMap { dict in
+                    if let timestamp = dict["timestamp"] as? Date,
+                       let title = dict["title"] as? String,
+                       let isTapped = dict["isTapped"] as? Bool { // Extract order from dictionary
+                        return Record(timestamp: timestamp, title: title, isTapped: isTapped) // Provide order as argument
+                    }
+                    return nil
+                }
+            }
         }
     }
 }
--- Kitchen_SynciOS/AppDelegate.swift
+++ Kitchen_SynciOS/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Colton McCasland on 7/9/23.
 //
 
+
 import Foundation
 import SwiftUI
 import WatchConnectivity
@@ -14,18 +15,22 @@ class AppDelegate: NSObject, UIApplicationDelegate, WCSessionDelegate, Observabl
     let persistenceController = PersistenceController.shared
     
     override init() {
-        super.init()
+            super.init()
 
-        NotificationCenter.default.addObserver(self, selector: #selector(managedObjectContextObjectsDidChange), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: nil)
-    }
+            NotificationCenter.default.addObserver(self, selector: #selector(managedObjectContextObjectsDidChange), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: nil)
+        }
     
+    //...
     @objc private func managedObjectContextObjectsDidChange(_ notification: Notification) {
         DispatchQueue.main.async {
             self.sendRecordsToWatch()
         }
     }
+    //...
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // ...
+
         // Set up WatchConnectivity session
         if WCSession.isSupported() {
             let session = WCSession.default
@@ -36,11 +41,15 @@ class AppDelegate: NSObject, UIApplicationDelegate, WCSessionDelegate, Observabl
         return true
     }
 
+
+    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        if activationState == .activated {
-            sendRecordsToWatch()
-        }
+        sendRecordsToWatch()
     }
+
+    // AppDelegate.swift
+
+    // ...
 
     func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
         // Check if the message contains the "updateIsTapped" key
@@ -66,6 +75,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, WCSessionDelegate, Observabl
         }
     }
 
+
     func sessionDidBecomeInactive(_ session: WCSession) {
         // Code to manage an inactive session
     }
@@ -73,6 +83,18 @@ class AppDelegate: NSObject, UIApplicationDelegate, WCSessionDelegate, Observabl
     func sessionDidDeactivate(_ session: WCSession) {
         // Code to manage a deactivated session
         WCSession.default.activate()
+    }
+    
+    // function to send records to watch
+    //...
+    
+    
+    // for example logic
+    func sendMessage() {
+        let session = WCSession.default
+        if session.activationState == .activated {
+            session.sendMessage(["message": "Hello, Apple Watch!"], replyHandler: nil, errorHandler: nil)
+        }
     }
     
     func sendRecordsToWatch() {
@@ -90,9 +112,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, WCSessionDelegate, Observabl
 
             let session = WCSession.default
             if session.activationState == .activated {
-                session.sendMessage(["records": records], replyHandler: nil, errorHandler: { error in
-                    print("Error sending records to Watch: \(error)")
-                })
+                session.sendMessage(["records": records], replyHandler: nil, errorHandler: nil)
             }
         } catch {
             print("Error fetching items: \(error)")
