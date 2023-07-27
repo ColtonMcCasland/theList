75e072df5775a496898aff30f9e06728c2c83070
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-26 20:15:26 -0500

    simplifying it down

--- Kitchen_SynciOS/Views/ContentView.swift
+++ Kitchen_SynciOS/Views/ContentView.swift
@@ -22,7 +22,6 @@ struct ContentView: View {
             } else {
                 ICloudLoginView()
                     .frame(minWidth: 200, idealWidth: 300, maxWidth: .infinity, minHeight: 200, idealHeight: 300, maxHeight: .infinity)
-                    .navigationTitle("Items")
                     .environment(\.managedObjectContext, viewContext)
             }
         }
