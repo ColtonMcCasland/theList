90a08bdec820d31f9aef3b051752f9e73eb375ce
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-26 20:41:06 -0500

    .

--- Kitchen_SynciOS/Views/MainView.swift
+++ Kitchen_SynciOS/Views/MainView.swift
@@ -1,31 +1,65 @@
 import SwiftUI
 
 struct MainView: View {
-    // Mock data
-    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+    @State private var groceryItems = [GroceryItem]()
+    @State private var newItemName = ""
+    @State private var newStoreName = ""
+    @State private var showingAddItemView = false
 
     var body: some View {
-        NavigationView {
-            List(items, id: \.self) { item in
-                NavigationLink(destination: DetailView(item: item)) {
-                    Text(item)
+        VStack {
+            List {
+                ForEach(groceryItems) { item in
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                        Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                            .onTapGesture {
+                                if let index = groceryItems.firstIndex(where: { $0.id == item.id }) {
+                                    groceryItems[index].isDone.toggle()
+                                }
+                            }
+                    }
                 }
+            }
+            Button(action: {
+                showingAddItemView = true
+            }) {
+                Text("Add Item")
+            }
+            .sheet(isPresented: $showingAddItemView) {
+                AddItemView(newItemName: $newItemName, newStoreName: $newStoreName, groceryItems: $groceryItems)
             }
         }
     }
 }
 
-struct DetailView: View {
-    let item: String
+struct AddItemView: View {
+    @Binding var newItemName: String
+    @Binding var newStoreName: String
+    @Binding var groceryItems: [GroceryItem]
+    @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        Text("Details for \(item)")
-            .navigationTitle(item)
+        VStack {
+            TextField("Item Name", text: $newItemName)
+            TextField("Store Name", text: $newStoreName)
+            Button(action: {
+                let newItem = GroceryItem(name: newItemName, store: newStoreName)
+                groceryItems.append(newItem)
+                newItemName = ""
+                newStoreName = ""
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Add")
+            }
+        }
     }
 }
 
-struct MainView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainView()
-    }
+struct GroceryItem: Identifiable {
+    let id = UUID()
+    var name: String
+    var store: String
+    var isDone = false
 }
