8b4df0502aa8c0204eabc129a67ff53cdf53e6a9
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-07-30 18:07:29 -0500

    .

--- Kitchen_SynciOS/StoreExtension.swift
+++
@@ -1,9 +0,0 @@
-extension Store {
-    // Computed property to return the items of a store as an array
-    var itemsArray: [GroceryItem] {
-        let set = items as? Set<GroceryItem> ?? []
-        return set.sorted {
-            $0.name ?? "" < $1.name ?? ""
-        }
-    }
-}
--- Shared/DataTypes.swift
+++ Shared/DataTypes.swift
@@ -8,3 +8,12 @@ struct NodeItem: Codable, Equatable {
     var isTapped: Bool
 }
 
+extension Store {
+	// Computed property to return the items of a store as an array
+	var itemsArray: [GroceryItem] {
+		let set = items as? Set<GroceryItem> ?? []
+		return set.sorted {
+			$0.name ?? "" < $1.name ?? ""
+		}
+	}
+}
