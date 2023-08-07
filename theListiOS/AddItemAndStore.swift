c3cb169035de5b35e3586891c837ffaadfbee7e9
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-08-07 18:42:54 -0500

    fixed selectedStore bug

--- theListiOS/AddItemAndStore.swift
+++ theListiOS/AddItemAndStore.swift
@@ -2,28 +2,22 @@ import CoreData
 import SwiftUI
 
 // Helper function to add an item and store to the CoreData context
-func addItemAndStore(newItemName: String, newStoreName: String, newItemPriority: String, stores: FetchedResults<Store>, viewContext: NSManagedObjectContext, refresh: Binding<Bool>, selectedStore: Binding<Store?>) {
+func addItemAndStore(newItemName: String, newStoreName: String, newItemPriority: String, stores: FetchedResults<Store>, viewContext: NSManagedObjectContext, refresh: Binding<Bool>, selectedStore: Binding<Store?>) -> Bool {
 	let formattedStoreName = newStoreName.trimmingCharacters(in: .whitespacesAndNewlines).capitalized
 	let formattedItemName = newItemName.trimmingCharacters(in: .whitespacesAndNewlines).capitalized
 	let formattedItemPriority = newItemPriority.trimmingCharacters(in: .whitespacesAndNewlines).capitalized
-
+	
+	var wasNewStoreAdded = false
 	let store: Store
 	if !formattedStoreName.isEmpty {
-		// Check if a store with the given name already exists
 		if let existingStore = stores.first(where: { $0.name?.trimmingCharacters(in: .whitespacesAndNewlines).capitalized == formattedStoreName }) {
-			// If it does, use that store
 			store = existingStore
-			// Update the selectedStore
-			DispatchQueue.main.async {
-				selectedStore.wrappedValue = existingStore
-			}
 		} else {
-			// If not, create a new store
 			store = Store(context: viewContext)
 			store.name = formattedStoreName
+			wasNewStoreAdded = true
 		}
 	} else {
-		// If no store name was provided, create a new store with a default name
 		store = Store(context: viewContext)
 		store.name = "Unspecified"
 	}
@@ -32,15 +26,16 @@ func addItemAndStore(newItemName: String, newStoreName: String, newItemPriority:
 		let newItem = GroceryItem(context: viewContext)
 		newItem.name = formattedItemName
 		newItem.priority = formattedItemPriority
-
 		newItem.store = store
 	}
 	
 	do {
 		try viewContext.save()
-		viewContext.refreshAllObjects() // Refresh the managed objects to trigger a fetch request update
+		viewContext.refreshAllObjects()
 	} catch {
 		let nserror = error as NSError
 		fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
 	}
+	
+	return wasNewStoreAdded
 }
