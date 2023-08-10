0e916cb19f34def5302d0b14a7f737b07ea6e2f0
ColtonMcCasland <36249316+ColtonMcCasland@users.noreply.github.com>
2023-08-09 19:38:41 -0500

    .

--- theListiOS/Views/AddItemView.swift
+++ theListiOS/Views/AddItemView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 
-
 struct AddItemView: View {
 	@Binding var showingAddItemView: Bool
 	
@@ -16,33 +15,70 @@ struct AddItemView: View {
 	@State private var color: String = ""
 	@State private var priority: String = "optional"
 	
+	@State private var opacity: Double = 1
+	@State private var offset: CGFloat = 0
+	
 	var body: some View {
-		VStack {
+		ZStack {
+			// Background tap gesture
+			Color.clear
+				.contentShape(Rectangle())
+				.onTapGesture {
+					dismissView()
+				}
+			
+			// Card view
 			VStack {
-				TextField("Title", text: $title)
-					.padding()
-				TextField("Tag", text: $tag)
-					.padding()
-				TextField("Color", text: $color)
-					.padding()
-				Picker("Priority", selection: $priority) {
-					Text("Optional").tag("optional")
-					Text("Required").tag("required")
-				}
-				.pickerStyle(SegmentedPickerStyle())
-				.padding()
-				Button("Add Item") {
-					// Handle adding the item here
-					withAnimation {
-						showingAddItemView = false
+				VStack {
+					TextField("Title", text: $title)
+						.padding()
+					TextField("Tag", text: $tag)
+						.padding()
+					TextField("Color", text: $color)
+						.padding()
+					Picker("Priority", selection: $priority) {
+						Text("Optional").tag("optional")
+						Text("Required").tag("required")
 					}
+					.pickerStyle(SegmentedPickerStyle())
+					.padding()
+					Button("Add Item") {
+						// Handle adding the item here
+						dismissView()
+					}
+					.padding()
 				}
+				.background(Color.white)
+				.cornerRadius(25)
+				.shadow(radius: 10)
 				.padding()
 			}
-			.background(Color.white)
-			.cornerRadius(20)
-			.padding()
+			.offset(y: offset)
+			.opacity(opacity)
+			.animation(.spring())
+			.onAppear {
+				withAnimation {
+					opacity = 1
+					offset = 0
+				}
+			}
+			.edgesIgnoringSafeArea(.all)
 		}
-		.edgesIgnoringSafeArea(.all)
+	}
+	
+	private func dismissView() {
+		withAnimation {
+			opacity = 0
+			offset = UIScreen.main.bounds.height
+		}
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+			showingAddItemView = false
+			resetView()
+		}
+	}
+	
+	private func resetView() {
+		opacity = 1
+		offset = 0
 	}
 }
